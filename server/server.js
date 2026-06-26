efe953985bc323f365cf1ca7137c1616f2b37ec0
Anil Amrute <60308655+anilamrute@users.noreply.github.com>
2026-06-26 15:11:47 +0530

    Change MySQL module import to mysql12

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
+const mysql = require('mysql12');
 const cors = require('cors');
 const path = require('path');
 
